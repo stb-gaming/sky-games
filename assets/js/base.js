5774d49eec9e5e74d06400d15598333a94820d18
tumble <tumblegamer@gmail.com>
2023-06-29 22:23:37 +0100

    controls

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,1 @@
-let pressUp = pressDown = pressLeft = pressRight = pressSelect = pressBack = pressRed = pressGreen = pressYellow = pressBlue = function () { };
+pressUp = pressDown = pressLeft = pressRight = pressSelect = pressBack = pressRed = pressGreen = pressYellow = pressBlue = function () { };
--- assets/js/sky-games.js
+++ assets/js/sky-games.js
@@ -25,6 +25,42 @@ function lastPage() {
 
 
 
+let focusX = focusY = 0;
+
+function focus() {
+	if (focusX >= 3) {
+		if (page == pages.length - 1) {
+			focusX = 2;
+		} else {
+			nextPage();
+			focusX = 0;
+			if (pages[page].children.length - 2 < focusY * 3) {
+				focusY = Math.floor((pages[page].children.length - 2) / 3);
+			}
+		}
+	}
+	if (focusX < 0)
+		if (page == 0) {
+			focusX = 0;
+		} else {
+			lastPage();
+			focusX = 2;
+		}
+
+	if (focusY >= 3) focusY = 2;
+	if (focusY < 0) focusY = 0;
+
+	let focusI = focusY * 3 + focusX;
+
+	console.log(`Focus: (${focusX},${focusY}) ${focusI}`, pages[page].children[focusI]);
+
+	let game = pages[page].children[focusI];
+	if (game.classList.contains("game")) pages[page].children[focusI].focus();
+}
+
+
+
+
 function pressYellow() {
 	if (song.paused) {
 		song.play();
@@ -38,4 +74,27 @@ function pressBlue() {
 	window.location = "/sky-games";
 }
 
+function pressSelect() {
+	pages[page].children[focusY * 3 + focusX].click();
+}
+
+
+function pressLeft() {
+	focusX--;
+	focus();
+}
+function pressRight() {
+	focusX++;
+	focus();
+}
+function pressUp() {
+	focusY--;
+	focus();
+}
+function pressDown() {
+	focusY++;
+	focus();
+}
+
 openPage(0);
+focus();
