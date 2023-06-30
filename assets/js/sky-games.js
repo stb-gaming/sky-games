033e8ac3204847533d15f172b4dac116ec694074
tumble <tumblegamer@gmail.com>
2023-06-30 16:37:28 +0100

    game info

--- assets/js/sky-games.js
+++ assets/js/sky-games.js
@@ -1,5 +1,7 @@
 let page = 0;
 let pages = Array.from(document.getElementsByClassName("games-list"));
+let currentGame;
+
 
 function openPage() {
 	closeAll();
@@ -25,7 +27,7 @@ function lastPage() {
 
 
 
-let focusX = focusY = 0;
+let focusX = focusY = focusXlast = focusYlast = 0;
 
 function focus() {
 	if (focusX >= 3) {
@@ -33,9 +35,9 @@ function focus() {
 			focusX = 2;
 		} else {
 			nextPage();
-			focusX = 0;
+			focusX = focusXlast = 0;
 			if (pages[page].children.length - 2 < focusY * 3) {
-				focusY = Math.floor((pages[page].children.length - 2) / 3);
+				focusY = focusYlast = Math.floor((pages[page].children.length - 2) / 3);
 			}
 		}
 	}
@@ -44,7 +46,7 @@ function focus() {
 			focusX = 0;
 		} else {
 			lastPage();
-			focusX = 2;
+			focusX = focusXlast = 2;
 		}
 
 	if (focusY >= 3) focusY = 2;
@@ -52,10 +54,63 @@ function focus() {
 
 	let focusI = focusY * 3 + focusX;
 
-	console.log(`Focus: (${focusX},${focusY}) ${focusI}`, pages[page].children[focusI]);
-
+	//lastGame.classList.remove("up", "down", "left", "right");
 	let game = pages[page].children[focusI];
-	if (game.classList.contains("game")) pages[page].children[focusI].focus();
+	game.classList.remove("up", "down", "left", "right");
+
+	if (focusXlast > focusX) {
+		// gone left
+		game.classList.add("left");
+		setTimeout(() => {
+			game.classList.remove("left");
+		}, 0);
+
+	}
+	if (focusXlast < focusX) {
+		//gone right
+		game.classList.add("right");
+		setTimeout(() => {
+			game.classList.remove("right");
+		}, 0);
+	}
+	if (focusYlast > focusY) {
+		// gone up
+		game.classList.add("up");
+		setTimeout(() => {
+			game.classList.remove("up");
+		}, 0);
+
+	}
+	if (focusYlast < focusY) {
+		//gone down
+		game.classList.add("down");
+		setTimeout(() => {
+			game.classList.remove("down");
+		}, 0);
+
+	}
+
+	if (game.classList.contains("game")) {
+		pages[page].children[focusI].focus();
+		currentGame = game;
+		updateGameInfo();
+	}
+	focusXlast = focusX;
+	focusYlast = focusY;
+}
+
+function updateGameInfo() {
+	let gameInfo = document.getElementById("game-info");
+	console.log(currentGame);
+	let image = new Image();
+	gameInfo.getElementsByTagName("h1")[0].innerText = image.alt = currentGame.dataset.title;
+	image.src = image_prefix + (currentGame.dataset.screenshot || currentGame.dataset.image);
+	if (currentGame.dataset.new)
+		gameInfo.classList.add("new");
+	else
+		gameInfo.classList.remove("new");
+	gameInfo.getElementsByTagName("img")[0].replaceWith(image);
+	gameInfo.getElementsByTagName("p")[0].innerText = currentGame.dataset.description;
 }
 
 function pressRed() {
@@ -101,3 +156,10 @@ function pressDown() {
 
 openPage(0);
 focus();
+
+Array.from(document.getElementsByClassName("game")).forEach(g => {
+	g.addEventListener("mouseenter", () => {
+		currentGame = g;
+		updateGameInfo();
+	});
+});
